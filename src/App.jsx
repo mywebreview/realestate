99b9a9c196665d430042b595ad2029484ca4fca6
mywebreview <famuyiwaclinton4@gmail.com>
2024-06-20 23:01:40 +0100

    Pages and routes creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
 
-function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+
+
+export const App = () => {
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App
